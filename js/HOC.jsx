b1d411d6580333c689b83e659b6039ce750111c8
Sin Cheung Ko <scko823@gmail.com>
2016-09-03 10:36:40 -0700

    polish example for changing color component using HOC

--- js/HOC.jsx
+++ js/HOC.jsx
@@ -2,21 +2,50 @@ import React from 'react'
 //first create a stateless functional component
 //it does not have a this.state. It can only has props passed in
 //It is a pure function, just input props, output a component 
-const statelessButton = (props) => <button>{props.text}</button>
+const statelessButton = (props) => <button 
+																	style={{"width":"255px", 
+																	"height":"255px", 
+																	"backgroundColor":
+																	`rgb(${props.red},${props.green},${props.blue})`}} 
+																	onMouseMove={props.changeColor} 
+																	onMouseLeave={props.restoreColor} 
+																	className="btn">{props.text}</button>
 
 
-const HOCFactory = (InnerComponent) => class extends React.Component{
+export const HOCFactory = (InnerComponent) => class extends React.Component{
 	constructor(props){
 		super(props)
-		this.state={text:"wrapped"}
+		this.state={
+			text:"Move the mouse around",
+			red:"127",
+			green:"127",
+			blue:"127"
+		}
+		this.changeColor = this.changeColor.bind(this)
+		this.restoreColor = this.restoreColor.bind(this)
+	}
+	restoreColor(){
+		this.setState({"green": "127"})
+		this.setState({"blue": "127"})
+		this.setState({"red": "127"})
+	}
+	changeColor(e){
+		// console.log(e.clientX)
+		// console.log(e.clientY)
+		this.setState({"green": e.clientX+''})
+		this.setState({"blue": e.clientY+''})
+		console.log(this.state)
 	}
 	render(){
-		return <InnerComponent text={this.state.text} />
+		return <InnerComponent 
+			changeColor={this.changeColor} 
+			restoreColor={this.restoreColor}
+			{...this.state} />
 	}
 	componentDidMount(){
 		setTimeout(()=>{
 			// lexical this in fat arrow function in es6 :) 
-			this.setState({text:"wrapped and it has been 5 seconds"})
+			this.setState({text:"right for green, bottom for blue"})
 		},5000)
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,7 @@ module.exports = {
 			{
 				test:/\.jsx?$/,
 				exclude:'node_modules',
-				loader:'babel',
-				query:{
-					presets:['es2015','react']
-				}
+				loader:'babel'
 			}
 		]
 	}
